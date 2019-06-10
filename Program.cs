3bd2ea79dd182815529174e37d368bf486e6862b
Ariel Hammon <arielhammon@gmail.com>
2019-06-10 10:12:42 -0700

    Initial commit. Basic console app.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ACMEaccounting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to ACME Accounting Systems.");
+            Console.WriteLine("Remember, we're \"accounting\" on you!");
+            Console.ReadLine();
+        }
+    }
+}
